753bf3b3217623b49532a0dbe61937b65710fa6b
Adam Brauns <brauns.adam@gmail.com>
2022-07-27 22:53:14 -0500

    Added some comments and other code readability improvements

--- data_engineering_project.py
+++ data_engineering_project.py
@@ -12,9 +12,11 @@ def main(sqs, queue_url, conn, cursor):
   """
   Main function to run the program
   """
+  # Define function variables
   wait_sec = 10
   max_messages = 10
   time_wo_msg = 0
+
   while(1):
     try:
       # Receive messages in batches
@@ -27,12 +29,16 @@ def main(sqs, queue_url, conn, cursor):
       if (len(messages) > 1):
         time_wo_msg = 0
         batch_delete = []
+
+        # Parse the messages received
         for msg in messages['Messages']:
           try:
             usr_atr = json.loads(msg['Body'])
             cursor.execute("INSERT INTO user_logins (user_id, device_type, masked_ip, masked_device_id, locale, app_version, create_date) VALUES (%s, %s, %s, %s, %s, %s, %s)", (usr_atr['user_id'], usr_atr['device_type'], mask_data(usr_atr['ip']), mask_data(usr_atr['device_id']), usr_atr['locale'], usr_atr['app_version'].replace('.', ''), datetime.today().strftime('%Y-%m-%d')))
             batch_delete.append({'Id': msg['MessageId'], 'ReceiptHandle': msg['ReceiptHandle']})
             print('Wrote to database for batch item {0}'.format(len(batch_delete)))
+
+          # If data is missing a key, output key and remove it from queue
           except KeyError as e:
             print('Error: Data is missing data ({0}), removing from queue...'.format(e))
             sqs.delete_message(
@@ -40,11 +46,14 @@ def main(sqs, queue_url, conn, cursor):
               ReceiptHandle=msg['ReceiptHandle']
             )
 
-        if batch_delete:
+        # Remove the items in the batch
+        if (batch_delete):
           response = sqs.delete_message_batch(
                        QueueUrl=queue_url, 
                        Entries=batch_delete
                      )
+
+          # Verify the sqs deletion was successful prior to commiting the database changes
           if ('Successful' in response.keys()):
             conn.commit()
             print('Committed to database!')
@@ -52,9 +61,11 @@ def main(sqs, queue_url, conn, cursor):
             print('There was an issue removing the batch, canceling database commit.')
             logout(conn, cursor)
 
+      # If no new messages come in, increment timer and output message to user
       else:
         time_wo_msg = time_wo_msg + wait_sec
         print('Info: No new messages in {0} second(s)'.format(time_wo_msg))
+
     except Exception as e:
       print('Error: {0}'.format(e))
       exit_program(conn, cursor)
@@ -68,7 +79,7 @@ def sqs_connect():
   """
   try:
     sqs_params = config_parser('config.ini', 'sqs')
-    return [boto3.client('sqs', region_name=sqs_params['region_name'], endpoint_url=sqs_params['queue_url'], aws_access_key_id=sqs_params['aws_access_key_id'], aws_secret_access_key=sqs_params['aws_secret_access_key']), sqs_params['queue_url']]
+    return boto3.client('sqs', region_name=sqs_params['region_name'], endpoint_url=sqs_params['queue_url'], aws_access_key_id=sqs_params['aws_access_key_id'], aws_secret_access_key=sqs_params['aws_secret_access_key']), sqs_params['queue_url']
   except KeyError as e:
     print('SQS config missing parameter: {0}'.format(e))
     exit()
@@ -100,6 +111,7 @@ def config_parser(filename, section):
   parser = ConfigParser()
   parser.read(filename)
 
+  # Loop through config file to populate dictionary
   conf_file = {}
   if parser.has_section(section):
     params = parser.items(section)
