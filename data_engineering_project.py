f27ebb921a05c3b4d3b994a326e53cd4fd15a73e
Adam Brauns <brauns.adam@gmail.com>
2022-07-27 20:38:21 -0500

    Added config parser to obfuscate sqs config

--- data_engineering_project.py
+++ data_engineering_project.py
@@ -2,17 +2,48 @@
 
 # Import modules
 import boto3
+from configparser import ConfigParser
 
 def main():
   """
   Main function to run the program
   """
-  queue_url = 'http://localhost:4566/000000000000/login-queue'
-  sqs = boto3.client('sqs', region_name='localhost', endpoint_url=queue_url, aws_access_key_id='awslocalkeyid', aws_secret_access_key='awslocalkey')
+  [sqs, queue_url] = sqs_connect()
   message = sqs.receive_message(
     QueueUrl=queue_url,
   )
   print(message)
+
+def sqs_connect():
+  """
+  Connect to sqs queue
+
+  :return: sqs queue and queue url
+  """
+  try:
+    sqs_params = config_parser('config.ini', 'sqs')
+    return [boto3.client('sqs', region_name=sqs_params['region_name'], endpoint_url=sqs_params['queue_url'], aws_access_key_id=sqs_params['aws_access_key_id'], aws_secret_access_key=sqs_params['aws_secret_access_key']), sqs_params['queue_url']]
+  except KeyError as e:
+    print('SQS config missing parameter: {0}'.format(e))
+    exit()
+
+def config_parser(filename, section):
+  """
+  Parse config files
+
+  :return: dictionary of config params
+  """
+  parser = ConfigParser()
+  parser.read(filename)
+
+  conf_file = {}
+  if parser.has_section(section):
+    params = parser.items(section)
+    for param in params:
+      conf_file[param[0]] = param[1]
+  else:
+    raise Exception('File {0} does not contain section {1}'.format(filename, section))
+  return conf_file
 
 if __name__ == '__main__':
   main()
